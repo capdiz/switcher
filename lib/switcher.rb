87d54e91efb8c22d4aed8d952e39895d62f27dfd
Maurice Kusasirwa <capdiz@yahoo.com>
2021-11-09 20:21:39 +0300

    first commit

---
+++ lib/switcher.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "switcher/version"
+
+module Switcher
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/switcher_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Switcher do
+  it "has a version number" do
+    expect(Switcher::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
