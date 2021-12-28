f2cd6b0e29dad08d672167a6c47645855915ca36
Maurice Kusasirwa <capdiz@yahoo.com>
2021-12-28 18:10:39 +0300

    Services directory commit

--- lib/switcher/generators/service.rb
+++ lib/switcher/generators/service.rb
@@ -38,28 +38,27 @@ module Switcher
       def inside_motherdir?
         curr_dir = Dir.getwd
         if curr_dir != Dir.home
-          service_dir_exists?(curr_dir)
+          path = Pathname.new(curr_dir)
+          inside_motherdir = path.children.each do |child|
+            if child.directory? && child.basename.to_s == "services"
+              break true
+            end
+          end                    
+          
+          if inside_motherdir.class.to_s == "Array"
+            inside_motherdir = path.ascend do |dir|
+              break true if dir.directory? && dir.basename.to_s == "services"
+            end
+          end
+
+          if inside_motherdir.class.to_s == "Boolean"
+            return inside_motherdir
+          else
+            return false
+          end
         else
-        say("The switcher create service command needs to be called when inside a swither application (a single-mother directory). \n If you haven't created one yet, run switcher motherdir app_name", :green)
-        false
         end
       end
-
-      def service_dir_exists?(curr_dir)
-        path = Pathname.new(curr_dir)
-        service_path = path.children.each do |child|
-          break child if child.directory? && child.basename.to_s == "services"
-        end
-
-        if service_path.empty?
-          # check backwards for a services folder
-          service_path = path.ascend do |val|
-            file = Pathname.new(val)
-            break file if file.basename.to_s == "services"
-          end
-        end
-        service_path.empty?
-      end      
     end
   end
 end
