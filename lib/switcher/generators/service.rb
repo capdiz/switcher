755e61fdcbf049439d25eb37b55970aeb2a895a1
Maurice Kusasirwa <capdiz@yahoo.com>
2021-12-29 16:53:05 +0300

    file removal commit

--- lib/switcher/generators/service.rb
+++ lib/switcher/generators/service.rb
@@ -17,8 +17,7 @@ module Switcher
               files = Dir.entries("#{base_dir_name}").reject { |file| file == ".." || file == "." }
               unless files.size < 1
                 say(MESSAGES["output_msgs"]["files_to_remove"], :green)
-                dirs = files.map { |file_name| }.join(", ")
-                puts files
+                dirs = files.map { |file_name| file_name }.join(", ")
                 say(dirs, :blue)
                 response = ask(MESSAGES["queries"]["delete_services"], limited_to: OPTIONS)
                 unless response == "n"
